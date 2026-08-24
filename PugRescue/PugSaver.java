1e8f890eaf90661c6c670dd1e0ed9ac6a7da1572
LucasSkool <lyu1@hwemail.com>
2026-08-24 14:53:35 -0700

    First day
    1st da

---
+++ PugRescue/PugSaver.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PugSaver {
+
+	// Moves every dog whose breed is "Pug" in the list to the back of the list
+	public static void rescuePugs(ArrayList<Dog> list) {
+		int length = list.size();
+		for (int i = 0; i < length; i++) {
+			if (list.get(i).getBreed().equals("Pug")) {
+				list.add(list.get(i));
+				list.remove(i);
+				i--;
+				length--;
+			}
+		}
+	}
+}
